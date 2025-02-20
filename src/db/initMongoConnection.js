1e2537734c0bbe5d17f442feb0370d52735cbe7f
PavloBilenko <bilenko.pavlo@gmail.com>
2025-02-20 21:02:46 +0100

    -

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,9 +2,11 @@ import mongoose from 'mongoose';
 
 async function initMongoConnection() {
   try {
-    const mongoUrl = process.env.MONGODB_URL;
-    await mongoose.connect(mongoUrl);
-
+    await mongoose.connect(process.env.MONGODB_URL, {
+      user: process.env.MONGODB_USER,
+      pass: process.env.MONGODB_PASSWORD,
+      dbName: process.env.MONGODB_DB,
+    });
     console.log('✅ Mongo connection successfully established!');
   } catch (error) {
     console.error('❌ Error connecting to MongoDB:', error.message);
